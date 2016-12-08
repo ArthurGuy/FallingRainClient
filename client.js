afd33debe6217ea06a140ecdef3ad69ef22f4b57
Arthur Guy <arthur@arthurguy.co.uk>
2016-12-08 20:59:40 +0000

    Updated to fire event on start

--- client.js
+++ client.js
@@ -15,9 +15,14 @@ var port = new SerialPort("/dev/ttyAMA0", {
 
 port.on('open', function() {
     console.log('Serial port opened');
-    port.write('*s:1,1*', function () {
-
-    });
+    port.write('*S:0,0*');
+    port.write('*S:1,0*');
+    port.write('*S:2,0*');
+    port.write('*S:3,0*');
+    port.write('*S:4,0*');
+    port.write('*S:5,0*');
+    port.write('*S:6,0*');
+    port.write('*S:7,0*');
 });
 
 // open errors will be emitted as an error event
