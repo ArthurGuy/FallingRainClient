281f5c8c48dd1a5a4335d7b557e6cce6ba704275
Arthur Guy <arthur@arthurguy.co.uk>
2016-12-09 19:55:23 +0000

    Refactoring

--- client.js
+++ client.js
@@ -6,35 +6,64 @@ var port = new SerialPort("/dev/ttyAMA0", {
   baudRate: 115200
 });
 
+// Are we connected to the internet?
+var systemOnline = false;
 
-port.on('open', function() {
-    console.log('Serial port opened');
-    //Display an animation when the system boots
-    port.write('*S:0,0*');
-    port.write('*S:1,0*');
-    port.write('*S:2,0*');
-    port.write('*S:3,0*');
-    port.write('*S:4,0*');
-    port.write('*S:5,0*');
-    port.write('*S:6,0*');
-    port.write('*S:7,0*');
-});
+// Is the teensy connected?
+var displayConnected = false;
 
-// open errors will be emitted as an error event
-port.on('error', function(err) {
-  console.log('Error: ', err.message);
-});
+// Monitor the internet connection to see if we are online
+function checkOnlineStatus(){ 
+	isOnline(function(err, online) {
+		systemOnline = online;
+		setTimeout(startInternetChecks, 1000); 
+	});	
+}
 
-socket.on('connect', function () {
-    console.log('A socket connection was made');
-    socket.on('new-data', function(data){
+
+function init() {
+  checkOnlineStatus();
+  
+  // Connect to the serial port
+  port.on('open', function() {
+      console.log('Serial port opened');
+      //Display an animation when the system boots
+      port.write('*S:0,0*');
+      port.write('*S:1,0*');
+      port.write('*S:2,0*');
+      port.write('*S:3,0*');
+      port.write('*S:4,0*');
+      port.write('*S:5,0*');
+      port.write('*S:6,0*');
+      port.write('*S:7,0*');
+
+      displayConnected = true;
+  });
+
+  // open errors will be emitted as an error event
+  port.on('error', function(err) {
+    console.log('Error: ', err.message);
+    displayConnected = false;
+  });
+  
+  // Listen for websocket messages
+  
+  socket.on('connect', function(){
+		console.log('A socket connection was made');
+		socket.emit('display-connected', {}); 
+	});
+  
+  socket.on('new-data', function(data){
       var msg = '*S:' + data.x + ',' + data.y + '*';
-        port.write(msg, function(err) {
+      port.write(msg, function(err) {
             if (err) {
                 return console.log('Error on write: ', err.message);
             }
             console.log('new pixel update sent');
-        });
-        console.log(data);
-    });
-});
+      });
+      console.log(data);
+  });
+  
+}
+
+init();
